b7e90a66b010e8fa8993c7ff7d06ba70c7eddf3c
Francisco Perez <fperezfrancisco4@gmail.com>
2024-10-29 14:36:04 -0700

    adjusted review box size

--- src/components/carousel/SwiperButtons.jsx
+++ src/components/carousel/SwiperButtons.jsx
@@ -5,7 +5,7 @@ import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
 function SwiperButtons() {
   const swiper = useSwiper();
   return (
-    <div className="absolute z-20 m-auto top-auto bottom-6 inset-10 flex items-center justify-between p-4">
+    <div className="absolute z-20 m-auto top-auto bottom-4 inset-10 flex items-center justify-between p-4">
       <button
         onClick={() => swiper.slidePrev()}
         className="w-[36px] h-[36px] rounded-full bg-white flex items-center justify-center shadow-md hover:shadow-xl"
