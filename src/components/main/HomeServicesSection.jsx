d25d634191db41601801a3db16d31b66f1750234
Francisco Perez <fperezfrancisco4@gmail.com>
2024-10-30 22:20:46 -0700

    finished home services links

--- src/components/main/HomeServicesSection.jsx
+++ src/components/main/HomeServicesSection.jsx
@@ -5,6 +5,9 @@ import openGroupImg from "../../assets/trilogy-poles-square.webp";
 import smallGroupImg from "../../assets/small-trilogy.webp";
 
 function HomeServicesSection() {
+  const openInNewTab = (path) => {
+    window.open(path, "_blank");
+  };
   return (
     <div className="w-full py-[6rem] md:py-[10rem] lg:py-[14rem] px-4 md:px-8 flex flex-col items-center gap-10">
       <h2 className="sectionTitle text-[1.75rem] text-center lg:text-left md:text-[2.25rem] lg:text-[2.75rem] font-semibold text-[#1e1e1e] capitalize mb-0 leading-none">
@@ -16,18 +19,27 @@ function HomeServicesSection() {
           buttonTitle={"Register now"}
           img={openGroupImg}
           imgLink={"/training#program1"}
+          action={() =>
+            openInNewTab("https://trilogytoei.setmore.com/#classes")
+          }
         />
         <ServicesBoxSquare
           title="Private Mentorship"
           buttonTitle={"Apply now"}
           imgLink={"/training#program2"}
           img={privateImg}
+          action={() =>
+            openInNewTab(
+              "https://docs.google.com/forms/d/e/1FAIpQLSckSWoPR8wbdfO5V8wSpVr0hFU9qTfqUcA-ExQXG1hX6rQHjQ/viewform"
+            )
+          }
         />
         <ServicesBoxSquare
           title="Small Privates"
           buttonTitle={"Book now"}
           img={smallGroupImg}
           imgLink={"/training#program3"}
+          action={() => openInNewTab("https://trilogytoei.setmore.com/book")}
         />
       </div>
     </div>
