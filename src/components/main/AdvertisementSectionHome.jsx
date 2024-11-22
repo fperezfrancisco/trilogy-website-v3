1707d342c718ada2616ad4655f74ccf96c51aeb8
Francisco Perez <fperezfrancisco4@gmail.com>
2024-11-22 15:11:45 -0800

    website updated with winter programs

---
+++ src/components/main/AdvertisementSectionHome.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import winterPrograms from "../../assets/Winter-Programs.png";
+import prodigyFlyer from "../../assets/Prodigy-Flyer.png";
+import { useNavigate } from "react-router-dom";
+
+function AdvertisementSectionHome() {
+  const handleWinter = () => {
+    window.open(
+      "https://docs.google.com/forms/d/e/1FAIpQLScmafl-trO8VO01AFR6vZ2hqicHXjLdO4RcBdSaYlda5IXKAg/viewform",
+      "_blank"
+    );
+    return;
+  };
+
+  const handleProdigy = () => {
+    window.open(
+      "https://docs.google.com/forms/d/e/1FAIpQLSfPEV7poqRIA4fvRL9qiJnrv-F6aY1g-F_2dHGoGzPID1IFxA/viewform",
+      "_blank"
+    );
+    return;
+  };
+
+  const navigate = useNavigate();
+
+  return (
+    <div className="w-full py-[6rem] md:py-[10rem] lg:py-[14rem] px-4 md:px-8 flex flex-col items-center">
+      <h2 className="sectionTitle text-[1.75rem] text-center lg:text-left md:text-[2.25rem] lg:text-[2.75rem] font-semibold text-[#1e1e1e] capitalize mb-0 leading-none">
+        Join us this{" "}
+        <span className="text-blue-500 italic">Winter 2024/25</span>{" "}
+      </h2>
+      <div className="w-full flex flex-wrap gap-8 items-center my-8 justify-center">
+        <div className="w-full max-w-[600px] flex flex-col items-center">
+          {true && (
+            <img
+              src={winterPrograms}
+              className="object-cover w-full max-w-[600px] rounded-2xl overflow-hidden hover:scale-105 transition-all ease-out hover:shadow-2xl cursor-pointer"
+              alt=""
+              onClick={handleWinter}
+            />
+          )}
+          <button
+            className="p-2 border-2 border-black rounded-xl bg-neutral-300 hover:bg-neutral-400 w-full max-w-[350px] mt-4"
+            onClick={() => navigate("/winter")}
+          >
+            Learn More
+          </button>
+        </div>
+        <div className="w-full max-w-[600px] flex flex-col items-center">
+          {true && (
+            <img
+              src={prodigyFlyer}
+              className="object-cover w-full max-w-[600px] rounded-2xl overflow-hidden hover:scale-105 transition-all ease-out hover:shadow-2xl cursor-pointer"
+              alt=""
+              onClick={handleProdigy}
+            />
+          )}
+          <button
+            className="p-2 border-2 border-black rounded-xl bg-neutral-300 hover:bg-neutral-400 w-full max-w-[350px] mt-4"
+            onClick={() => navigate("/prodigy")}
+          >
+            Learn More
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AdvertisementSectionHome;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import HomeAboutSection from "../components/main/HomeAboutSection";
 import Footer from "../components/footer/Footer";
 import CtaSection from "../components/cta/CtaSection";
 import HomeServicesSection from "../components/main/HomeServicesSection";
+import AdvertisementSectionHome from "../components/main/AdvertisementSectionHome";
 
 function Home() {
   return (
@@ -13,6 +14,7 @@ function Home() {
       <Header />
       <main className="w-full max-w-[2400px] mx-auto mt-[56px] my-0 flex flex-col">
         <HeroSectionMain />
+        <AdvertisementSectionHome />
         <HomeReviewSection />
         <HomeAboutSection />
         <HomeServicesSection />
